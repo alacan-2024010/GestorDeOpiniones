37aa30821ac62f83a688a2671043c2b47a5a2074
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-17 14:49:21 -0600

    Se agregaron cambios

--- AuthServiceGestorOpiniones/configs/app.js
+++ AuthServiceGestorOpiniones/configs/app.js
@@ -10,6 +10,11 @@ export const initServer = async () => {
     const app = express();
     const PORT = process.env.PORT;
 
+    app.use(express.json());
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan('dev'));
+
     try {
         await dbConnection();
 
