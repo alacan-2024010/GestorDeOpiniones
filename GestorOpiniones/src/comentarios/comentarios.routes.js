73935f44067730e2bfda319fc41ac069078ef3a6
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-18 16:13:57 -0600

    Se agregaron las rutas de los comentarios para los endpoints

---
+++ GestorOpiniones/src/comentarios/comentarios.routes.js
@@ -0,0 +1,36 @@
+import Router from 'express';
+import { validateJWT } from '../../middlewares/validate-JWT.js';
+import { 
+    createComment, 
+    getYourComments, 
+    updateComment,
+    deleteComment 
+} from './comentarios.controller.js';
+
+const router = new Router;
+
+router.post(
+    '/create',
+    validateJWT,
+    createComment
+)
+
+router.get(
+    '/listar',
+    validateJWT,
+    getYourComments
+)
+
+router.put(
+    '/update/:id',
+    validateJWT,
+    updateComment
+)
+
+router.delete(
+    '/delete/:id',
+    validateJWT,
+    deleteComment
+)
+
+export default router;
--- GestorOpiniones/src/publicaciones/publicaciones.routes.js
+++ GestorOpiniones/src/publicaciones/publicaciones.routes.js
@@ -38,3 +38,5 @@ router.delete(
     validateJWT,
     deletePublication
 )
+
+export default router;
