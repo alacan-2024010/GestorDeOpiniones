5b2cbd00e3d3ea294139ca6a535a99817bf52175
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-18 16:18:34 -0600

    Se agregaron las rutas en el app.js

--- GestorOpiniones/configs/app.js
+++ GestorOpiniones/configs/app.js
@@ -4,7 +4,38 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './db.js';
 
+import publicationRoutes from '../src/publicaciones/publicaciones.routes.js';
+import commentRoutes from '../src/comentarios/comentarios.routes.js';
+
 const BASE_PATH = '/gestorOpiniones/v1';
+
+const middlewares = (app) => {
+    app.use(cors(corsOptions));
+    app.use(helmet(helmetConfiguration));;
+    app.use(morgan('dev'));
+}
+
+const routes = (app) => {
+
+    app.use(`${BASE_PATH}/publications`, publicationRoutes);
+    app.use(`${BASE_PATH}/comments`, commentRoutes);
+
+    app.get(`${BASE_PATH}/Health`, (request, response) => {
+        response.status(200).json({
+            status: 'Healthy',
+            timestamp: new Date().toISOString(),
+            service: 'GestorOpiniones Server'
+        })
+    })
+
+    app.use((req, res) => {
+        res.status(404).json({
+            success: false,
+            message: 'Endpoint no encontrado en la API'
+        })
+    })
+}
+
 
 export const initServer = async () => {
     const app = express();
@@ -13,6 +44,8 @@ export const initServer = async () => {
 
     try {
         await dbConnection();
+        middlewares(app);
+        routes(app);
 
         app.listen(PORT, () => {
             console.log(`GestorOpiniones Server running on port ${PORT}`);
